bded76ff55d94ec269a16fb228315975d797f2e9
sungslee <k8440009@gmail.com>
2020-11-25 15:24:23 +0900

    add ft_strcmp

--- ft_strcmp/ft_strcmp.s
+++ ft_strcmp/ft_strcmp.s
@@ -1,14 +1,55 @@
 section .text
-global _ft_strcmp
+global	_ft_strcmp
 
 _ft_strcmp:
-			cmp rdi, 0
-			jz done
-			cmp rsi, 0
-			jz done
+			xor		rcx, rcx
+			xor		rdx, rdx
+			xor		rax, rax
+			cmp		rdi, 0
+			jz		check_null
+			cmp		rsi, 0
+			jz		check_null
+			jmp		end_str
 
-compare:	cmp BYTE[rdi], 0
-			jz done
-			
+end_str:
+			cmp		BYTE[rdi + rcx], 0
+			jz		check_right
+			cmp		BYTE[rsi + rcx], 0
+			jz		check_left
+			jmp		compare
 
-done:
+compare:
+			mov		dl, BYTE[rdi + rcx]
+			cmp		dl, BYTE[rsi + rcx]
+			jg		left
+			jl		right
+			inc		rcx
+			jmp		end_str
+
+check_left:
+			cmp		BYTE[rdi + rcx], 0
+			jz		equal
+			jmp		left
+
+check_right:
+			cmp		BYTE[rsi + rcx], 0
+			jz		equal
+			jmp		right
+
+check_null:
+			cmp		rdi, rsi
+			jz		equal
+			jg		left
+			jmp		right
+
+equal:
+			mov		rax, 0
+			ret
+
+left:
+			mov		rax, -1
+			ret
+
+right:
+			mov		rax, 1
+			ret
--- ft_strcmp/test.c
+++ ft_strcmp/test.c
@@ -2,19 +2,19 @@
 
 int main()
 {
+	
 	printf("same : %d\n", ft_strcmp("123", "123"));
 	printf("left : %d\n", ft_strcmp("123", "12"));
 	printf("right : %d\n", ft_strcmp("12", "123"));
 
-	/*
+	
 	printf("same : %d\n", ft_strcmp("", ""));
 	printf("left : %d\n", ft_strcmp("1", ""));
 	printf("right : %d\n", ft_strcmp("", "1"));
-
-	char *s1, s2;
+	
+	char *s1;
+	char *s2;
 	printf("null1 : %d\n", ft_strcmp(s1, s2));
 	printf("null2 : %d\n", ft_strcmp(s1, "123"));
 	printf("null3 : %d\n", ft_strcmp("123", s2));
-	*/
-
 }
